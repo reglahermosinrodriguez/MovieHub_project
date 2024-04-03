ddc43852a6a95040785815f7b319fa8b7b4747a2
Regla Hermosín Rodríguez <rhermosinrodriguez@gmail.com>
2024-04-03 10:50:42 +0200

    Connect with Prisma

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
 import app from "./server";
 import config from "./config/config";
-import connect from "./db/db";
+import prisma from "./db/client";
 
+const PORT = config.app.PORT
 
-
-
-const PORT = config.app.PORT;
-connect().then(()=>
-
-
-
-app.listen(PORT, () => console.log(`Server is running port ${PORT}`))
-
-)
+app.listen(PORT, async () => {
+    try {
+      await prisma.$connect();
+      console.log(
+        (`Server is running on port ${PORT} and is connected to db MovieHub from Atlas`)
+      );
+    } catch (error) {
+      console.log("error to connect, try again");
+    }
+  });
